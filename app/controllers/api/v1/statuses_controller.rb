b8b8281e010138990075112ceb4b9833767454da
hina <hina@hinaloe.net>
2021-10-05 05:20:44 +0900

    Revert "社会性フィルター"
    This reverts commit 94218ead398c5df2650435dced43859041f01a41.

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -36,11 +36,12 @@ class Api::V1::StatusesController < Api::BaseController
 
   def create
     @status = PostStatusService.new.call(current_user.account,
-                                         text: 'にゃーん',
+                                         text: status_params[:status],
                                          thread: @thread,
+                                         status_params[:in_reply_to_id].blank? ? nil : Status.find(status_params[:in_reply_to_id]),
                                          media_ids: status_params[:media_ids],
                                          sensitive: status_params[:sensitive],
-                                         spoiler_text: !status_params[:spoiler_text].empty? ? 'にゃーん' : nil,
+                                         spoiler_text: status_params[:spoiler_text],
                                          visibility: status_params[:visibility],
                                          scheduled_at: status_params[:scheduled_at],
                                          application: doorkeeper_token.application,
