15c77743b82ca5ea882fbfbdc80116b76b2f0589
hina <hina@hinaloe.net>
2021-10-05 05:44:34 +0900

    Revert "image nyann"
    This reverts commit ac276a5a70ecbc84780e4c32c33257f79bb6f9d7.

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -7,28 +7,12 @@ class Api::V1::MediaController < Api::BaseController
   before_action :check_processing, except: [:create]
 
   def create
-    @media = MediaAttachment.find(40)#.deep_dup
-    @media.update({status_id: nil})
+    @media = current_account.media_attachments.create!(media_params)
     render json: @media, serializer: REST::MediaAttachmentSerializer
-
-    # render json: { error: 'Gone.' }, status: 410
-  #   @media = current_account.media_attachments.create!(media_params)
-  #   render json: @media, serializer: REST::MediaAttachmentSerializer
-  # rescue Paperclip::Errors::NotIdentifiedByImageMagickError
-  #   render json: file_type_error, status: 422
-  # rescue Paperclip::Error
-  #   render json: processing_error, status: 500
-  
-  #   @media_attachment = current_account.media_attachments.create!(media_attachment_params)
-  #   render json: @media_attachment, serializer: REST::MediaAttachmentSerializer
-  # rescue Paperclip::Errors::NotIdentifiedByImageMagickError
-  #   render json: file_type_error, status: 422
-  # rescue Paperclip::Error
-  #   render json: processing_error, status: 500
-  end
-
-  def show
-    render json: @media_attachment, serializer: REST::MediaAttachmentSerializer, status: status_code_for_media_attachment
+  rescue Paperclip::Errors::NotIdentifiedByImageMagickError
+    render json: file_type_error, status: 422
+  rescue Paperclip::Error
+    render json: processing_error, status: 500
   end
 
   def update
